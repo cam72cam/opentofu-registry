1d299be85bda8d22773b1760278f459080d1454b
Christian Mesh <christianmesh1@gmail.com>
2024-04-05 09:55:40 -0400

    Change debug
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- .github/workflows/bump-versions.sh
+++ .github/workflows/bump-versions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set +e
+
 if [ -z "${PREFIX}" ]; then
 	echo "Expecting \$PREFIX to be set"
 	exit 1
@@ -37,8 +39,6 @@ git add ./modules ./providers
 git config user.email "core@opentofu.org"
 git config user.name "OpenTofu Core Development Team"
 git commit -m "Automated bump of versions for providers and modules"
-
-git show HEAD
 
 # Racing with other jobs, try a few times to push changes
 for i in {0..30}; do
